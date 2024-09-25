61b79e69787c0ec85665d1cc07f62fc1884092c7
Ilya <illyaetoilysha@gmail.com>
2024-09-25 16:54:37 +0000

    Simple API adjustment

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', \App\Http\Controllers\GetController::class);
+});

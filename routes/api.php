ce5e1701854421408a97e2f77681ef75fcc68ead
Ilya <illyaetoilysha@gmail.com>
2024-09-25 17:29:17 +0000

    Implemented API Auth logic from Laravel Breeze and sanctum settings configured

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', \App\Http\Controllers\GetController::class);
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });

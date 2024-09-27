ccf442e46376e1d61b2c3f612e92258c7a86600f
Ilya <illyaetoilysha@gmail.com>
2024-09-27 15:22:08 +0000

    Added some test pages with tailwind styles and implemented a few Vue routes

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,4 +1,14 @@
 const routes = [
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('../Pages/Auth/Register.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../Pages/Auth/Login.vue')
+    }
 ];
 
 export default routes;
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -9,6 +9,6 @@
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 <body>
-    <div id="app"></div>
+<div id="app"></div>
 </body>
 </html>

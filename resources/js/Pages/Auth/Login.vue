2daef2b774777169e33d60db666761f5b0ff1306
Ilya <illyaetoilysha@gmail.com>
2024-09-28 15:36:44 +0000

    Fixed send form via @keydown.enter

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -44,7 +44,7 @@ const submit = () => {
             <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input v-model="form.password" @keydown.enter="submit" id="password" name="password" type="password"
+            <input v-model="form.password" @keydown.enter.prevent="submit" id="password" name="password" type="password"
                    autocomplete="current-password"
                    required=""
                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
